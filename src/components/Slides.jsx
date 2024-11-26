ef6d2ad0cb213024d01b3564852187d86db7e3d2
mmagna <moisesmagna@gmail.com>
2024-11-26 18:19:10 -0300

    Initial commit: Slides tutorial C

---
+++ src/components/Slides.jsx
@@ -0,0 +1,11 @@
+import Slides from './components/Slides'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <Slides />
+    </div>
+  )
+}
+
+export default App
